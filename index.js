d27b74f3f4c1c57c73e80661f6e1b4ee7c359677
TomerPeer <tomerpeer2002@gmail.com>
2019-07-16 19:17:04 +0300

    added ansible playbook

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ http.createServer( function(request, response) {
         break;
     case('GET'):  
         response.writeHead(200, { 'Content-Type':'text/plain' } );
-        response.write('hi yuvi\n');
+        response.write('hi Tomer\n');
         response.write('We have had '+postRequests+' POST requests');
         break;
     default:
